9345a7da8d82a26638b7a8f4c5f5fc698011f00d
LeTak <tasso@kougioumtzidis.de>
2023-06-24 18:59:05 +0200

    general update
    fixed game window.
    
    fixed velocity.
    
    fixed respawn.
    
    game window grid size need fine tuning
    
    collision with obstacle still broken

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pygame
 import sys
 import random
 import math
-
+GRID_CELL_SIZE = 50  # Size of each cell in the grid
+SCREEN_WIDTH = 1920  # Width of the screen in pixels
+SCREEN_HEIGHT = 1020  # Height of the screen in pixels
 
 class Level:
     def __init__(self, grid_size):
@@ -38,19 +40,40 @@ class Level:
 
 class Banana:
     def __init__(self):
-        self.position = (0, 0)  # Initial position
+        self.position = (random.randint(0, SCREEN_WIDTH-100), random.randint(0, SCREEN_HEIGHT-100))
         self.velocity = (0, 0)  # Initial velocity
         self.is_golden = False  # By default, the banana is not golden
 
     def shoot(self, mouse_drag):
         # Calculate the new velocity based on the mouse drag
         # This is a simple implementation, you may need to adjust it based on your game's physics
-        self.velocity = (-mouse_drag[0], -mouse_drag[1])
+        scaling_factor = 0.01  # Adjust this value to change the speed of the banana
+        self.velocity = (-mouse_drag[0] * scaling_factor, -mouse_drag[1] * scaling_factor)
 
     def update_position(self):
-        # Update the banana's position based on its velocity
-        # This is a simple implementation, you may need to adjust it based on your game's physics
+        # Calculate the new position
+        new_position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
+
+        # Check if the new position would be outside the screen
+        if new_position[0] < 0 or new_position[0] > SCREEN_WIDTH:
+            # If it would be, reverse the x-velocity to make the banana bounce off the edge
+            self.velocity = (-self.velocity[0], self.velocity[1])
+        if new_position[1] < 0 or new_position[1] > SCREEN_HEIGHT:
+            # If it would be, reverse the y-velocity to make the banana bounce off the edge
+            self.velocity = (self.velocity[0], -self.velocity[1])
+
+        # Update the position
         self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
+
+        # Apply damping (reduce the velocity)
+        damping_factor = 0.99  # Adjust this value to change how quickly the banana slows down
+        self.velocity = (self.velocity[0] * damping_factor, self.velocity[1] * damping_factor)
+
+    def respawn(self):
+        # Set the position to a random point within the screen boundaries
+        self.position = (random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT))
+        # Reset the velocity
+        self.velocity = (0, 0)
 
 
 class Obstacle:
@@ -115,11 +138,11 @@ class Menu:
 class Game:
     def __init__(self):
         pygame.init()  # Initialize Pygame
-        self.screen = pygame.display.set_mode((800, 600))  # Create the Pygame window
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create the Pygame window
         self.clock = pygame.time.Clock()  # Create a clock object to control the frame rate
         self.current_banana = Banana()
         self.player = Player()
-        self.level = Level(grid_size=10)
+        self.level = Level(grid_size=8)
         self.menu = Menu()
 
     def start_game(self):
@@ -140,45 +163,60 @@ class Game:
 
     def check_collision(self):
         # Check if the banana has collided with an obstacle or the box
-        if self.current_banana.position in self.level.obstacle_positions:
-            return 'obstacle'
-        elif self.current_banana.position == self.level.box_position:
+        for obstacle_position in self.level.obstacle_positions:
+            # Convert the obstacle's position from grid cells to pixels
+            obstacle_position_pixels = (obstacle_position[0] * GRID_CELL_SIZE, obstacle_position[1] * GRID_CELL_SIZE)
+            distance = math.sqrt((self.current_banana.position[0] - obstacle_position_pixels[0]) ** 2 + (
+                        self.current_banana.position[1] - obstacle_position_pixels[1]) ** 2)
+            if distance < GRID_CELL_SIZE:
+                return 'obstacle'
+        # Convert the box's position from grid cells to pixels
+        box_position_pixels = (self.level.box_position[0] * GRID_CELL_SIZE, self.level.box_position[1] * GRID_CELL_SIZE)
+        distance = math.sqrt((self.current_banana.position[0] - box_position_pixels[0]) ** 2 + (
+                    self.current_banana.position[1] - box_position_pixels[1]) ** 2)
+        if distance < GRID_CELL_SIZE:
             return 'box'
-        else:
-            return None
+        return None
 
     def update_score(self, points):
         self.player.update_score(points)
 
     def generate_level(self):
-        self.level = Level(grid_size=10)
+        self.level = Level(grid_size=15)
         self.level.generate_box()
         self.level.generate_obstacles(num_obstacles=5)
+        self.current_banana.respawn()
 
     def draw_banana(self):
-        pygame.draw.circle(self.screen, (255, 255, 0), self.current_banana.position, 10)
+        pixel_position = (self.current_banana.position[0], self.current_banana.position[1])
+        pygame.draw.circle(self.screen, (255, 255, 0), pixel_position, 10)
 
     def draw_box(self):
-        pygame.draw.rect(self.screen, (255, 0, 0),
-                         pygame.Rect(self.level.box_position[0], self.level.box_position[1], 50, 50))
+        pixel_position = (self.level.box_position[0] * GRID_CELL_SIZE, self.level.box_position[1] * GRID_CELL_SIZE)
+        pygame.draw.rect(self.screen, (255, 0, 0), pygame.Rect(pixel_position[0], pixel_position[1], 50, 50))
 
     def draw_obstacles(self):
         for obstacle_position in self.level.obstacle_positions:
-            pygame.draw.rect(self.screen, (0, 255, 0), pygame.Rect(obstacle_position[0], obstacle_position[1], 50, 50))
+            pixel_position = (obstacle_position[0] * GRID_CELL_SIZE, obstacle_position[1] * GRID_CELL_SIZE)
+            pygame.draw.rect(self.screen, (0, 255, 0), pygame.Rect(pixel_position[0], pixel_position[1], 50, 50))
 
     def game_loop(self):
-        while self.menu.game_state == 'continue game':
+        mouse_down_pos = None  # Variable to store the position where the mouse button was pressed
+        running = True
+        while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.menu.game_state = 'end game'
+                    print("QUIT event triggered")
+                    running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    # Get the mouse position when the user clicks
-                    mouse_pos = pygame.mouse.get_pos()
-                    self.current_banana.position = mouse_pos
+                    # Store the mouse position when the user clicks
+                    mouse_down_pos = pygame.mouse.get_pos()
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    # Get the mouse position when the user releases the mouse button
-                    mouse_pos = pygame.mouse.get_pos()
-                    self.current_banana.shoot(mouse_pos)
+                    # Calculate the mouse drag
+                    mouse_drag = (
+                        pygame.mouse.get_pos()[0] - mouse_down_pos[0], pygame.mouse.get_pos()[1] - mouse_down_pos[1])
+                    # Shoot the banana
+                    self.current_banana.shoot(mouse_drag)
 
             # Update the banana's position
             self.current_banana.update_position()
@@ -186,8 +224,9 @@ class Game:
             # Check for collisions
             collision = self.check_collision()
             if collision == 'obstacle':
+                print("Obstacle collision detected")
                 # If the banana hit an obstacle, end the game
-                self.end_game()
+                running = False
             elif collision == 'box':
                 # If the banana hit the box, update the score and generate a new level
                 self.update_score(1 if not self.current_banana.is_golden else 5)
@@ -204,5 +243,7 @@ class Game:
 
 if __name__ == "__main__":
     game = Game()
+    game.menu.display_menu()
+    game.menu.start_new_game(game)
     game.start_game()
 
