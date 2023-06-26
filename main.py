7d4c8c70be7976729db6b17e1bcbb9cf992fb6cb
LeTak <tasso@kougioumtzidis.de>
2023-06-26 12:24:29 +0200

    Menu and respawn fixes, still broken

--- main.py
+++ main.py
@@ -40,10 +40,20 @@ class Level:
 
 
 class Banana:
-    def __init__(self):
-        self.position = (random.randint(0, SCREEN_WIDTH-100), random.randint(0, SCREEN_HEIGHT-100))
+    def __init__(self, shots, obstacle_positions, box_position):
+        self.position = self.generate_position(obstacle_positions, box_position)
         self.velocity = (0, 0)  # Initial velocity
-        self.is_golden = False  # By default, the banana is not golden
+        self.is_golden = shots <= 1 and random.randint(1, 100) == 1
+
+    def generate_position(self, obstacle_positions, box_position):
+        position = (random.randint(0, SCREEN_WIDTH-100), random.randint(0, SCREEN_HEIGHT-100))
+        while position in obstacle_positions or position == box_position:
+            position = (random.randint(0, SCREEN_WIDTH-100), random.randint(0, SCREEN_HEIGHT-100))
+        return position
+
+    def respawn(self, obstacle_positions, box_position):
+        self.position = self.generate_position(obstacle_positions, box_position)
+        self.velocity = (0, 0)
 
     def shoot(self, mouse_drag):
         # Calculate the new velocity based on the mouse drag
@@ -69,12 +79,6 @@ class Banana:
         # Apply damping (reduce the velocity)
         damping_factor = 0.99  # Adjust this value to change how quickly the banana slows down
         self.velocity = (self.velocity[0] * damping_factor, self.velocity[1] * damping_factor)
-
-    def respawn(self):
-        # Set the position to a random point within the screen boundaries
-        self.position = (random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT))
-        # Reset the velocity
-        self.velocity = (0, 0)
 
 
 class Obstacle:
@@ -121,15 +125,44 @@ class Player:
 
 
 class Menu:
-    def __init__(self):
+    def __init__(self, game):
         self.game_state = 'new game'  # Initial game state
+        self.game = game
 
-    def display_menu(self):
-        # Display the menu
-        # This will depend on your game's user interface
-        print('1. Start New Game')
-        print('2. Continue Game')
-        print('3. Display Scoreboard')
+    def display_menu(self, screen):
+        running = True
+        font = pygame.font.Font(None, 36)  # Choose the font for the text
+        start_game_text = font.render("Start New Game", 1,
+                                      (255, 255, 255))  # Create the text surface for "Start New Game"
+        continue_game_text = font.render("Continue Game", 1,
+                                         (255, 255, 255))  # Create the text surface for "Continue Game"
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    # Check if the mouse click was within the bounds of the "Start New Game" button
+                    if 100 <= mouse_pos[0] <= 300 and 100 <= mouse_pos[1] <= 200:
+                        self.game_state = 'new game'
+                        running = False  # Exit the menu loop
+                    # Check if the mouse click was within the bounds of the "Continue Game" button
+                    elif 100 <= mouse_pos[0] <= 300 and 300 <= mouse_pos[1] <= 400:
+                        self.game_state = 'continue game'
+                        running = False  # Exit the menu loop
+            # Draw the menu
+            screen.fill((0, 0, 0))  # Clear the screen
+            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(100, 100, 200, 100))  # Draw the "Start New Game" button
+            pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(100, 300, 200, 100))  # Draw the "Continue Game" button
+            screen.blit(start_game_text, (150, 130))  # Draw the "Start New Game" text
+            screen.blit(continue_game_text, (150, 330))  # Draw the "Continue Game" text
+            pygame.display.flip()  # Update the display
+
+        # After the menu loop is exited, start the game loop based on the game state
+        if self.game_state == 'new game':
+            self.game.start_game()
+        elif self.game_state == 'continue game':
+            self.game.resume_game()
 
     def start_new_game(self, game):
         # Start a new game
@@ -148,13 +181,14 @@ class Menu:
 
 class Game:
     def __init__(self):
-        pygame.init()  # Initialize Pygame
+        # Initialize Pygame
+        pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create the Pygame window
         self.clock = pygame.time.Clock()  # Create a clock object to control the frame rate
-        self.current_banana = Banana()
-        self.player = Player()
+        self.player = Player()  # Create a player
         self.level_time = 100  # Initial level time
         self.level = Level(grid_size=8)
+        self.current_banana = Banana(self.player.shots, self.level.obstacle_positions, self.level.box_position)
         self.menu = Menu()
 
     def start_game(self):
@@ -169,6 +203,9 @@ class Game:
         # Shoot the banana
         self.current_banana.shoot(mouse_drag)
         self.player.update_shots()  # Increase the shot count by 1
+        # If the shot count is bigger than 1, make the banana not golden
+        if self.player.shots > 1:
+            self.current_banana.is_golden = False
 
     def resume_game(self):
         if self.menu.game_state == 'paused':
@@ -182,10 +219,11 @@ class Game:
         # Check if the banana has collided with an obstacle or the box
         for obstacle_position in self.level.obstacle_positions:
             # Convert the obstacle's position from grid cells to pixels
-            obstacle_position_pixels = (obstacle_position[0] * GRID_CELL_SIZE, obstacle_position[1] * GRID_CELL_SIZE)
-            distance = math.sqrt((self.current_banana.position[0] - obstacle_position_pixels[0]) ** 2 + (
-                        self.current_banana.position[1] - obstacle_position_pixels[1]) ** 2)
-            if distance < GRID_CELL_SIZE:
+            obstacle_position_pixels = ((obstacle_position[0] * GRID_CELL_SIZE) + GRID_CELL_SIZE // 2,
+                                        (obstacle_position[1] * GRID_CELL_SIZE) + GRID_CELL_SIZE // 2)
+            distance = math.sqrt((self.current_banana.position[0] - obstacle_position_pixels[0]) ** 2 +
+                                 (self.current_banana.position[1] - obstacle_position_pixels[1]) ** 2)
+            if distance < GRID_CELL_SIZE // 2:
                 return 'obstacle'
         # Convert the box's position from grid cells to pixels
         box_position_pixels = (self.level.box_position[0] * GRID_CELL_SIZE, self.level.box_position[1] * GRID_CELL_SIZE)
@@ -209,13 +247,16 @@ class Game:
         self.level = Level(grid_size=15)
         self.level.generate_box()
         self.level.generate_obstacles(num_obstacles=5)
-        self.current_banana.respawn()
+        self.current_banana = Banana(self.player.shots, self.level.obstacle_positions, self.level.box_position)  # Create a new banana
+        self.current_banana.respawn(self.level.obstacle_positions, self.level.box_position)  # Respawn the banana
         self.level_time = 100  # Reset the level time
         self.player.shots = 0  # Reset the shot count
 
     def draw_banana(self):
         pixel_position = (self.current_banana.position[0], self.current_banana.position[1])
-        pygame.draw.circle(self.screen, (255, 255, 0), pixel_position, 10)
+        # If the banana is golden, draw it in orange, otherwise draw it in yellow
+        color = (255, 165, 0) if self.current_banana.is_golden else (255, 255, 0)
+        pygame.draw.circle(self.screen, color, pixel_position, 10)
 
     def draw_box(self):
         pixel_position = (self.level.box_position[0] * GRID_CELL_SIZE, self.level.box_position[1] * GRID_CELL_SIZE)
@@ -223,8 +264,13 @@ class Game:
 
     def draw_obstacles(self):
         for obstacle_position in self.level.obstacle_positions:
-            pixel_position = (obstacle_position[0] * GRID_CELL_SIZE, obstacle_position[1] * GRID_CELL_SIZE)
-            pygame.draw.rect(self.screen, (0, 255, 0), pygame.Rect(pixel_position[0], pixel_position[1], 50, 50))
+            # Convert the obstacle's position from grid cells to pixels
+            pixel_position = ((obstacle_position[0] * GRID_CELL_SIZE) + GRID_CELL_SIZE // 2,
+                              (obstacle_position[1] * GRID_CELL_SIZE) + GRID_CELL_SIZE // 2)
+            pygame.draw.rect(self.screen, (0, 255, 0),
+                             pygame.Rect(pixel_position[0] - GRID_CELL_SIZE // 2,
+                                         pixel_position[1] - GRID_CELL_SIZE // 2,
+                                         GRID_CELL_SIZE, GRID_CELL_SIZE))
 
     def draw_text(self):
         font = pygame.font.Font(None, 36)  # Choose the font for the text
@@ -287,7 +333,7 @@ class Game:
 
 if __name__ == "__main__":
     game = Game()
-    game.menu.display_menu()
-    game.menu.start_new_game(game)
-    game.start_game()
+    game.menu.display_menu(game.screen)
+   # game.menu.start_new_game(game)
+    #game.start_game()
 
